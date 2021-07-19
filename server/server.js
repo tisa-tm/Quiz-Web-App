599b3e665ce8e47b040fdc19cc6859a424e50a3c
tisa-tm <57312874+tisa-tm@users.noreply.github.com>
2021-07-19 11:50:02 +0545

    backend

--- server/server.js
+++ server/server.js
@@ -1,20 +1,16 @@
 const express = require('express');
-require('dotenv').config();
+require('dotenv').config({ path: require('find-config')('.env') });
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb');
-
-// const connection = require('./connection');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-// app.use(connection.connection());
 
 mongoose
-  .connect("mongodb+srv://admin:LRs8p3lsplGwSsp0@quiz.wp8vv.mongodb.net/Quiz?retryWrites=true&w=majority", {
+  .connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
